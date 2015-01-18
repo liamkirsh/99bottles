6a3b7b2fd19a4fd994babca92077f30c12b00edb
Liam Kirsh <liam.kirsh@riseup.net>
2015-01-18 00:15:31 -0800

    setting up mandrill

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,18 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+   
+   config.action_mailer.smtp_settings = {
+      :address    => "smtp.mandrillapp.com",
+      :port       => 587,
+      :user_name  => ENV["MANDRILL_USERNAME"],
+      :password   => ENV["MANDRILL_APIKEY"]
+   }
+
+   # ActionMailer Config
+   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+   config.action_mailer.delivery_method = :smtp
+   config.action_mailer.perform_deliveries = true
+   config.action_mailer.raise_delivery_errors = true 
+
 end
