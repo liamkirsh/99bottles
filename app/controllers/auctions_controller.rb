94b186e6b71e67b0d632b19cbd0dc02ec92442b5
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-01-18 01:21:58 -0800

    create file where single auction item is shown

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -2,4 +2,8 @@ class AuctionsController < ApplicationController
   def index
     @auctions = Auction.where(live: true)
   end
+
+  def show
+    @auction = Auction.find_by(id: params[:id])
+  end
 end
