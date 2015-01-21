bfd8081a6d8d7bd9bd58b43595986bccd8de393b
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-01-20 23:55:33 -0800

    when order is created the winning price that the user bid is set.

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,14 +2,19 @@ class Order < ActiveRecord::Base
   belongs_to :auction
   belongs_to :user
 
-  validate :auction_is_dead, on: :create
+  validate :auction_is_dead, :set_winning_price, on: :create
 
-  #create only if auction is dead
+  def product
+    self.auction.product
+  end
+
+  private
+
   def auction_is_dead
     errors.add(:auction_id, "Auction is still live") unless self.auction.is_dead?
   end
-  
-  def product
-    self.auction.product
+
+  def set_winning_price
+    self.winning_price = Bid.where(user: self.user_id, auction: self.auction_id).order("offer_price DESC").first.offer_price
   end
 end
