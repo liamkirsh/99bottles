d22a433e03ff07552a8015d43ff89ff69402b96b
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-01-22 16:45:03 -0800

    actionMailer working with hardcoded values

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,6 @@
 class UserMailer < ApplicationMailer
+  default from: "heyimuzz@gmail.com"
+  def auction_won(user)
+    mail(to: "faraaznishtar@gmail.com", subject: "Coungratulations! You won!")
+  end
 end
